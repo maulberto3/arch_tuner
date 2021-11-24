8fcf09bd48e37f31f0440f2c810888d62e45ef98
Mauricio Maroto <maulberto3@hotmail.com>
2021-11-24 13:20:27 -0600

    first commit

--- regular.py
+++ regular.py
@@ -30,8 +30,8 @@ from pathlib import Path
 
 # check the TODO's below
 
-# manual_seed(16)
-# seed(16)
+manual_seed(16)
+seed(16)
 
 class ARCH_NET(nn.Module):
     def __init__(
@@ -45,69 +45,24 @@ class ARCH_NET(nn.Module):
         
         self.input_size = input_size
         self.hidden_dim = hidden_dim
-        self.nodes = nodes
-        self.activ = [F.selu, F.relu, F.celu, F.elu, sigmoid, F.logsigmoid, F.softplus, F.softsign, tanh]
         
-        self.start = 'ab'
-        self.options = list(self.start)
-        
-        self.init_layers = nn.ModuleDict()
-        self.init_activs = {}
-        self.helper = OrderedDict()
-        self.helper2 = OrderedDict()
-        self.hidden_layers = nn.ModuleDict()
-        self.hidden_activs = {}
-
-        self.create_random_arch()
-        
-        self.to_output = nn.Linear(hidden_dim, output_dim)
-        
-    def create_initial_layer(self):
-        for init in self.start:
-            self.init_layers[init] = nn.Linear(self.input_size, self.hidden_dim)
-            self.init_activs[init] = choice(self.activ)
-
-    def create_node(self, options):
-        pair = list(combinations(options, 2))
-        pair = choice(pair)
-        #pair =  "".join(pair)
-        if pair in self.helper:
-            pair = self.create_node(options)
-        else:
-            self.helper[pair] = str(len(self.hidden_layers))
-            self.hidden_layers[str(len(self.hidden_layers))] = nn.Linear(self.hidden_dim, self.hidden_dim)
-            self.hidden_activs[str(len(self.hidden_layers))] = choice(self.activ)
-            self.helper2[pair] = self.hidden_activs[str(len(self.hidden_layers))].__name__
-        return pair
-            
-    def create_random_arch(self):
-        self.create_initial_layer()
-        options = self.options.copy()
-        for i in range(self.nodes):
-            pair = self.create_node(options)
-            options.append(pair)
-        #print('Net Architecture is')
-        #pprint(self)
-
-        # def create_arch_from(self, arch):
-
+        self.simple_arch = nn.Sequential(
+            nn.Linear(input_size, hidden_dim),
+            nn.Dropout(p=0.2),
+            nn.Sigmoid(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.Dropout(p=0.2),
+            nn.Softsign(),
+            # nn.Linear(hidden_dim, hidden_dim),
+            # nn.Dropout(p=0.2),
+            # nn.Softsign(),
+            nn.Linear(hidden_dim, output_dim),
+            nn.LogSoftmax(dim=-1))
         
     def forward(self, x):
-        init_l = {lay[0]: act[1](F.dropout(lay[1](x), p=0.2)) for lay, act in zip(
-                self.init_layers.items(),
-                self.init_activs.items())}
-        hidden_l = {}
-        for a, b, c  in zip(
-                self.helper.items(),
-                self.hidden_layers.items(),
-                self.hidden_activs.items()):
-            hidden_l[a[0]] = {**init_l, **hidden_l}[a[0][0]] * {**init_l, **hidden_l}[a[0][1]]
-            hidden_l[a[0]] = c[1](F.dropout(b[1](hidden_l[a[0]]), p=0.2))
-        hidden_l = stack(list(hidden_l.values()))
-        hidden_l = hidden_l.sum(dim=0)
-        hidden_l = self.to_output(hidden_l)
-        hidden_l = F.log_softmax(hidden_l, dim=-1).squeeze()
-        return hidden_l
+        x = self.simple_arch(x).squeeze(1)
+        # print(x.shape)
+        return x
     
 # net = ARCH_NET()
 # net
@@ -137,20 +92,15 @@ test = load(Path.home() / 'data' / 'MNIST' / 'processed' / 'test.pt')
 
 archs = []
 
-for i in range(200):
-    print(i)
+for _ in range(1):
     # Net
-    net = ARCH_NET(hidden_dim=64, nodes=6).train()
-    net.create_random_arch()
+    net = ARCH_NET(hidden_dim=256, nodes=10).train()
+    # net.create_random_arch()
     net = net.cuda()
 
-    # Arch
-    # pprint(net.helper)
-    # pprint(net.helper2)
-
-    # print(f'{sum([numel(sublayer) for layer in net.init_layers.values() for sublayer in layer.parameters()]):,}')
-    # print(f'{sum([numel(sublayer) for layer in net.hidden_layers.values() for sublayer in layer.parameters()]):,}')
-    # print(list(net.named_parameters()))
+    #f'{sum([numel(sublayer) for layer in net.init_layers.values() for sublayer in list(layer.parameters())]):,}'
+    #f'{sum([numel(sublayer) for layer in net.layers.values() for sublayer in list(layer.parameters())]):,}'
+    # list(net.named_parameters())
     
     optimizer = Adam(net.parameters(), lr=LR)
     criterion = nn.NLLLoss()
@@ -161,7 +111,8 @@ for i in range(200):
                         batch_size=BATCH_SIZE,
                         shuffle=True,
                         drop_last=True)
-    for epoch in range(1):
+
+    for epoch in range(5):
         #running_loss = []
         train_correct = 0
         train_total = 0
@@ -207,18 +158,4 @@ for i in range(200):
             test_correct += (predicted == labels).sum().item()
         print(f'TEST {test_correct / test_total * 100:.2f} %')
             
-    archs.extend(list(zip(net.helper.values(), net.helper2.items(), repeat(100 * test_correct / test_total))))
-
-t = DataFrame(archs)
-t1 = t[0].astype(str)
-t1 = t1.str.split(',', expand=True)
-t2 = concat([t1, t], axis=1)
-# print(t)
-print(t.shape)
-t2.to_csv('temp2.csv', index=False)
-
-# print(t2.groupby(2).mean().squeeze())
-# print(t2.groupby(2).std().squeeze())
-
-# print(t2.groupby(6).mean().squeeze())
-# print(t2.groupby(6).std().squeeze())
+    # archs.append([list(zip(net.helper, net.helper2.values())), 100 * test_correct / test_total])
